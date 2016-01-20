4606c58fc3a5aaca0caeae3882e3d308c23593bc
Adam Booth <adam@ambooth.com>
2016-01-20 12:39:43 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# React Component Starter
+
+![Screenshot](https://github.com/adam8810/react-component-starter/blob/master/screenshot.png)
+
+Comes packaged with Twitter Bootstrap and Font Awesome icons to get you up and going.
+
+## Installation
+
+     git clone git@github.com:adam8810/react-component-starter.git
+     cd react-component-starter
+     npm install
+
+## Usage
+
+     npm start
+
+We use webpack-dev-server to auto-reload upon changes. Navigate to `localhost:8080` in your browser after running command.
---
+++ src/index.jsx
@@ -0,0 +1,61 @@
+import React      from 'react';
+import { render } from 'react-dom';
+
+require ('expose?$!expose?jQuery!jquery');
+require ('bootstrap-webpack');
+require ("font-awesome-webpack");
+
+// Blueprint Component
+const WrapperElement = React.createClass ({
+  render () {
+    return (
+      <div className="outer-wrapper">
+        <div className="component-wrapper">{this.props.children}</div>
+      </div>
+    );
+  }
+});
+
+const Component = React.createClass ({
+  // React Life Cycle Methods
+  // https://facebook.github.io/react/docs/component-specs.html#lifecycle-methods
+  
+  componentDidMount () {
+    // If you want to integrate with other JavaScript frameworks,
+    // set timers using setTimeout or setInterval, or send AJAX
+    // requests, perform those operations in this method.
+
+  },
+
+  componentWillUnmount () {
+    // Clean up anything that was created in componentDidMount
+    // such as timers or other elements
+
+  },
+
+  getInitialState () {
+    // Use this for creating state. Ex: Setting visibility of element
+    // or default value for a checkbox
+    return {
+      buttonColor: 'green'
+    };
+  },
+  render () {
+    const buttonColor = {
+      backgroundColor: this.state.buttonColor
+    };
+
+    return (
+      <div>
+        <WrapperElement>
+          <h1>Component Blueprint</h1>
+          <button style={buttonColor} className="btn btn-primary"><i className="fa fa-check"></i> Bootstrap Button</button >
+          </WrapperElement>
+      </div>
+    );
+  }
+});
+
+const node = document.getElementById ('component');
+
+render (<Component />, node);
